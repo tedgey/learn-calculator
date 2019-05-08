dfd4978d2e5641eef08ef843ad35f5b44b986a70
Sean R Reid <sean@torchcodelab.com>
2019-05-08 11:33:23 -0400

    Updated the instructions in the scripts comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 "use strict";
 
-var input = document.getElementById('input'), // input/output button
+const input = document.getElementById('input'), // input/output button
     number = document.querySelectorAll('.numbers div'), // number buttons
     operator = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
-    clear = document.getElementById('clear'), // clear button
-    resultDisplayed = false; // flag to keep an eye on what output is displayed
+    clear = document.getElementById('clear'); // clear button
+    
+let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
 
+// adding click handlers to the operation buttons
 
 // on click of 'equal' button
-
 
 // clearing the input on press of clear
